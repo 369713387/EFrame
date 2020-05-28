ff34dbec0d0277a16b51769e3560ad1a0cce0a18
369713387 <369713387@qq.com>
2020-05-28 18:33:40 +0800

    工具加入：FSM——有限状态机

---
+++ Assets/EFrame/Tools/EFrame.FSM.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFrame
+{
+    /// <summary>
+    /// FSM
+    /// </summary>
+    /// <typeparam name="TStateEnum">状态枚举</typeparam>
+    /// <typeparam name="TEventEnum">事件枚举</typeparam>
+    public class FSM<TStateEnum, TEventEnum> : IDisposable
+    {
+        private Action<TStateEnum, TStateEnum> mOnStateChanged = null;
+
+        public FSM(Action<TStateEnum, TStateEnum> onStateChanged = null)
+        {
+            mOnStateChanged = onStateChanged;
+        }
+
+        /// <summary>
+		/// FSM状态机状态改变回调.
+		/// </summary>
+		public delegate void FSMOnStateChagned(params object[] param);
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public class FSMState<TName>
+        {
+            public TName Name;
+
+            public Action EnterCallback; //回调函数
+
+            public Action ExitCallback; //回调函数
+
+            public FSMState(TName name, Action enterCallback = null , Action exitCallback = null)
+            {
+                Name = name;
+                EnterCallback = enterCallback;
+                ExitCallback = exitCallback;
+            }
+
+            /// <summary>
+            /// 状态转换关系字典
+            /// </summary>
+            public readonly Dictionary<TEventEnum, FSMTranslation<TName, TEventEnum>> TranslationDict =
+                new Dictionary<TEventEnum, FSMTranslation<TName, TEventEnum>>();
+        }
+
+        /// <summary>
+        /// 状态转换
+        /// </summary>
+        /// <typeparam name="TStateName">状态名</typeparam>
+        /// <typeparam name="KEventName">事件名</typeparam>
+        public class FSMTranslation<TStateName, KEventName>
+        {
+            public TStateName FromState;
+            public KEventName Name;
+            public TStateName ToState;
+            public Action<object[]> OnTranslationCallback; // 回调函数,执行的状态事件
+
+            public FSMTranslation(TStateName fromState, KEventName name, TStateName toState,
+                Action<object[]> onStateChagned)
+            {
+                FromState = fromState;
+                ToState = toState;
+                Name = name;
+                OnTranslationCallback = onStateChagned;
+            }
+        }
+
+        /// <summary>
+        /// 当前状态
+        /// </summary>
+        TStateEnum mCurState;
+
+        /// <summary>
+        /// 当前状态
+        /// </summary>
+        public TStateEnum State
+        {
+            get { return mCurState; }
+        }
+
+        /// <summary>
+        /// 状态字典
+        /// </summary>
+        Dictionary<TStateEnum, FSMState<TStateEnum>> mStateDic = new Dictionary<TStateEnum, FSMState<TStateEnum>>();
+
+
+        /// <summary>
+        /// 添加状态
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="enter"></param>
+        public void AddState(TStateEnum name, Action enterCallback = null, Action exitCallback = null)
+        {
+            if (!mStateDic.ContainsKey(name))
+            {
+                mStateDic[name] = new FSMState<TStateEnum>(name);
+
+                if (enterCallback != null)
+                {
+                    mStateDic[name].EnterCallback = enterCallback;
+                }
+
+                if (enterCallback != null)
+                {
+                    mStateDic[name].ExitCallback = exitCallback;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 添加状态
+        /// </summary>
+        /// <param name="fsmState"></param>
+        public void AddState(FSMState<TStateEnum> fsmState)
+        {
+            if (!mStateDic.ContainsKey(fsmState.Name))
+            {
+                mStateDic[fsmState.Name] = fsmState;
+            }
+        }
+
+        /// <summary>
+        /// 添加状态转换关系
+        /// </summary>
+        /// <param name="fromState">起始状态</param>
+        /// <param name="name">事件名字</param>
+        /// <param name="toState">目标状态</param>
+        /// <param name="onStateChagned">回调函数</param>
+        public void AddTransition(TStateEnum fromState, TEventEnum name, TStateEnum toState, Action<object[]> onStateChagned = null)
+        {
+            mStateDic[fromState].TranslationDict[name] = new FSMTranslation<TStateEnum, TEventEnum>(fromState, name, toState, onStateChagned);
+        }
+
+        /// <summary>
+        /// 添加状态转换关系
+        /// </summary>
+        /// <param name="fromState">起始状态</param>
+        /// <param name="name">事件名字</param>
+        /// <param name="toState">目标状态</param>
+        /// <param name="onStateChagned">回调函数</param>
+        public void AddTransition(FSMTranslation<TStateEnum, TEventEnum> fsmTranslation)
+        {
+            if (mStateDic.ContainsKey(fsmTranslation.FromState) && mStateDic.ContainsKey(fsmTranslation.ToState))
+            {
+                mStateDic[fsmTranslation.FromState].TranslationDict[fsmTranslation.Name] = fsmTranslation;
+            }
+        }
+
+        /// <summary>
+        /// 启动状态机，以目标状态
+        /// </summary>
+        /// <param name="name">状态名</param>
+        public void Start(TStateEnum name)
+        {
+            mCurState = name;
+        }
+
+        /// <summary>
+        /// 处理当前状态下的事件
+        /// </summary>
+        /// <param name="name">状态名字</param>
+        /// <param name="param">参数组</param>
+        public void HandleEvent(TEventEnum name, params object[] param)
+        {
+            if (mCurState != null && mStateDic[mCurState].TranslationDict.ContainsKey(name))
+            {
+                var tempTranslation = mStateDic[mCurState].TranslationDict[name];
+
+                if (tempTranslation.OnTranslationCallback != null)
+                {
+                    tempTranslation.OnTranslationCallback.Invoke(param);
+                }
+
+                if (mOnStateChanged != null)
+                {
+                    mOnStateChanged.Invoke(mCurState, tempTranslation.ToState);
+                }
+
+                if (mStateDic[mCurState].ExitCallback != null)
+                {
+                    mStateDic[mCurState].ExitCallback.Invoke();
+                }
+
+                mCurState = tempTranslation.ToState;
+
+                if (mStateDic[mCurState].EnterCallback != null)
+                {
+                    mStateDic[mCurState].EnterCallback.Invoke();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 销毁状态机
+        /// </summary>
+        void Clear()
+        {
+            foreach (var keyValuePair in mStateDic)
+            {
+                foreach (var translationDictValue in keyValuePair.Value.TranslationDict.Values)
+                {
+                    translationDictValue.OnTranslationCallback = null;
+                }
+
+                keyValuePair.Value.TranslationDict.Clear();
+            }
+
+            mStateDic.Clear();
+        }
+
+        /// <summary>
+        /// 状态机销毁
+        /// </summary>
+        public void Dispose()
+        {
+            Clear();
+        }
+    }
+}
---
+++ Assets/EFrameExample/EFrameExample_FSM.cs
@@ -0,0 +1,196 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EFrame.Example
+{
+    public enum PlayerState
+    {
+        Idle,
+        Run,
+        Attack,
+        Dead
+    }
+
+    public enum PlayerEvent
+    {
+        Idle,
+        Run,
+        Attack,
+        Dead
+    }
+
+    /// <summary>
+    /// FSM示例代码
+    /// 状态定义：1.站立，2.跑步，3.攻击，4.死亡
+    /// 状态转换：1.站立->跑步，2.跑步->站立，3.站立->攻击，
+    ///           4.攻击->站立，5.攻击->死亡，6.站立->死亡
+    ///           7.跑步->死亡
+    /// </summary>
+    public class EFrameExample_FSM : MonoBehaviour
+    {
+        //创建状态机
+        FSM<PlayerState, PlayerEvent> fsm = new FSM<PlayerState, PlayerEvent>(
+            (playerState, playerEvent) => { Debug.Log("状态改变"); }
+            );
+
+        //创建状态
+        FSM<PlayerState, PlayerEvent>.FSMState<PlayerState> idleState = 
+            new FSM<PlayerState, PlayerEvent>.FSMState<PlayerState>(
+                PlayerState.Idle
+                , () => { Debug.Log("进入站立状态"); }
+                , () => { Debug.Log("退出站立状态"); }
+                );
+        FSM<PlayerState, PlayerEvent>.FSMState<PlayerState> runState =
+            new FSM<PlayerState, PlayerEvent>.FSMState<PlayerState>(
+                PlayerState.Run
+                , () => { Debug.Log("进入跑步状态"); }
+                , () => { Debug.Log("退出跑步状态"); }
+                );
+        FSM<PlayerState, PlayerEvent>.FSMState<PlayerState> attackState =
+            new FSM<PlayerState, PlayerEvent>.FSMState<PlayerState>(
+                PlayerState.Attack
+                , () => { Debug.Log("进入攻击状态"); }
+                , () => { Debug.Log("退出攻击状态"); }
+                );
+        FSM<PlayerState, PlayerEvent>.FSMState<PlayerState> deadState =
+            new FSM<PlayerState, PlayerEvent>.FSMState<PlayerState>(
+                PlayerState.Dead
+                , () => { Debug.Log("进入死亡状态"); }
+                , () => { Debug.Log("退出死亡状态"); }
+                );
+
+        //创建状态转换
+        FSM<PlayerState, PlayerEvent>.FSMTranslation<PlayerState,PlayerEvent> idle2run =
+            new FSM<PlayerState, PlayerEvent>.FSMTranslation<PlayerState, PlayerEvent>(
+                PlayerState.Idle
+                ,PlayerEvent.Run
+                ,PlayerState.Run
+                ,RunEvent
+                );
+
+        FSM<PlayerState, PlayerEvent>.FSMTranslation<PlayerState, PlayerEvent> run2idle =
+            new FSM<PlayerState, PlayerEvent>.FSMTranslation<PlayerState, PlayerEvent>(
+                PlayerState.Run
+                , PlayerEvent.Idle
+                , PlayerState.Idle
+                , IdleEvent
+                );
+
+        FSM<PlayerState, PlayerEvent>.FSMTranslation<PlayerState, PlayerEvent> idle2attack =
+            new FSM<PlayerState, PlayerEvent>.FSMTranslation<PlayerState, PlayerEvent>(
+                PlayerState.Idle
+                , PlayerEvent.Attack
+                , PlayerState.Attack
+                , AttackEvent
+                );
+
+        FSM<PlayerState, PlayerEvent>.FSMTranslation<PlayerState, PlayerEvent> attack2idle =
+            new FSM<PlayerState, PlayerEvent>.FSMTranslation<PlayerState, PlayerEvent>(
+                PlayerState.Attack
+                , PlayerEvent.Idle
+                , PlayerState.Idle
+                , IdleEvent
+                );
+
+        FSM<PlayerState, PlayerEvent>.FSMTranslation<PlayerState, PlayerEvent> attack2dead =
+            new FSM<PlayerState, PlayerEvent>.FSMTranslation<PlayerState, PlayerEvent>(
+                PlayerState.Attack
+                , PlayerEvent.Dead
+                , PlayerState.Dead
+                , DeadEvent
+                );
+
+        FSM<PlayerState, PlayerEvent>.FSMTranslation<PlayerState, PlayerEvent> run2dead =
+            new FSM<PlayerState, PlayerEvent>.FSMTranslation<PlayerState, PlayerEvent>(
+                PlayerState.Run
+                , PlayerEvent.Dead
+                , PlayerState.Dead
+                , DeadEvent
+                );
+
+        FSM<PlayerState, PlayerEvent>.FSMTranslation<PlayerState, PlayerEvent> idle2dead =
+            new FSM<PlayerState, PlayerEvent>.FSMTranslation<PlayerState, PlayerEvent>(
+                PlayerState.Idle
+                , PlayerEvent.Dead
+                , PlayerState.Dead
+                , DeadEvent
+                );
+
+        //添加状态
+        void AddState()
+        {
+            fsm.AddState(idleState);
+            fsm.AddState(runState);
+            fsm.AddState(attackState);
+            fsm.AddState(deadState);
+        }
+
+        //添加转换关系
+        void AddTranslation()
+        {
+            fsm.AddTransition(idle2run);
+            fsm.AddTransition(run2idle);
+            fsm.AddTransition(idle2attack);
+            fsm.AddTransition(attack2idle);
+            fsm.AddTransition(run2dead);
+            fsm.AddTransition(idle2dead);
+            fsm.AddTransition(attack2dead);
+        }
+
+        //开启状态机
+        private void Start()
+        {
+            AddState();
+            AddTranslation();
+            fsm.Start(PlayerState.Idle);            
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                fsm.HandleEvent(PlayerEvent.Idle);
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                fsm.HandleEvent(PlayerEvent.Run);
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                fsm.HandleEvent(PlayerEvent.Attack);
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                fsm.HandleEvent(PlayerEvent.Dead);
+            }
+        }
+
+        #region 状态事件
+
+        static void IdleEvent(object[] parm)
+        {
+            Debug.Log("执行站立事件");
+        }
+
+        static void RunEvent(object[] parm)
+        {
+            Debug.Log("执行跑步事件");
+        }
+
+        static void AttackEvent(object[] parm)
+        {
+            Debug.Log("执行攻击事件");
+        }
+
+        static void DeadEvent(object[] parm)
+        {
+            Debug.Log("执行死亡事件");
+        }
+        #endregion
+
+    }
+
+}
+
+
