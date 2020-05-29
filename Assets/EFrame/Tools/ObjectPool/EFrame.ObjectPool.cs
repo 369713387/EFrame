3cdbe08dbd7d9695214a7f4c8657c3db03ad7d47
369713387 <369713387@qq.com>
2020-05-29 15:35:04 +0800

    添加对象池工具

---
+++ Assets/EFrame/Tools/ObjectPool/EFrame.ObjectPool.cs
@@ -0,0 +1,289 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace EFrame
+{
+    #region 对象池抽象类
+
+    /// <summary>
+    /// 观察者数量
+    /// </summary>
+    public interface ICountObserveAble
+    {
+        int CurCount { get; }
+    }
+
+    /// <summary>
+    /// 对象工厂
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IObjectFactory<T>
+    {
+        T Create();
+    }
+
+    public interface IPool<T>
+    {
+        /// <summary>
+        /// 分配一个对象
+        /// </summary>
+        /// <returns></returns>
+        T Allocate();
+
+        /// <summary>
+        /// 回收一个对象
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        bool Recycle(T obj);
+    }
+
+    /// <summary>
+    /// 对象池抽象类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class Pool<T> : IPool<T>, ICountObserveAble
+    {
+        /// <summary>
+        /// 获取观察者数量
+        /// </summary>
+        public int CurCount
+        {
+            get
+            {
+                return mCacheStack.Count;
+            }
+        }
+
+        protected IObjectFactory<T> mFactory;
+
+        protected readonly Stack<T> mCacheStack = new Stack<T>();
+
+        /// <summary>
+        /// 对象池最大容量
+        /// </summary>
+        protected int mMaxCount = 24;
+
+        /// <summary>
+        /// 申请一个对象
+        /// </summary>
+        /// <returns></returns>
+        public virtual T Allocate()
+        {
+            return mCacheStack.Count == 0
+                ? mFactory.Create()
+                : mCacheStack.Pop();
+        }
+
+        /// <summary>
+        /// 回收一个对象
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public abstract bool Recycle(T obj);
+    }
+    #endregion
+
+    #region 简易对象池的实现
+
+    public class SimpleObjcetFactor<T> : IObjectFactory<T>
+    {
+        public SimpleObjcetFactor(Func<T> FactoryMethod)
+        {
+            mFactoryMethod = FactoryMethod;
+        }
+
+        protected Func<T> mFactoryMethod;
+
+        public T Create()
+        {
+            return mFactoryMethod();
+        }
+    }
+
+
+    public class SimpleObjectPool<T> : Pool<T>
+    {
+        protected Action<T> mResetMethod;
+
+        public SimpleObjectPool(Func<T> factoryMethod, Action<T> resetMethod = null,int initCount = 0)
+        {
+            mFactory = new SimpleObjcetFactor<T>(factoryMethod);
+            mResetMethod = resetMethod;
+
+            for (int i = 0; i < initCount; i++)
+            {
+                mCacheStack.Push(mFactory.Create());
+            }
+
+        }
+
+
+        public override bool Recycle(T obj)
+        {
+            if(mResetMethod != null)
+            {
+                mResetMethod.Invoke(obj);
+            }
+
+            mCacheStack.Push(obj);
+
+            return true;
+        }
+    }
+
+    #endregion
+
+    #region 安全的对象池的实现
+
+    public class DefaultObjectFactory<T> : IObjectFactory<T> where T : new()
+    {
+        public T Create()
+        {
+            return new T();
+        }
+    }
+
+    /// <summary>
+    /// 池对象类型约束
+    /// </summary>
+    public interface IPoolable
+    {
+        /// <summary>
+        /// 接收重置事件
+        /// </summary>
+        void OnRecycled();
+
+        /// <summary>
+        /// 该对象是否被回收过
+        /// </summary>
+        bool IsRecycled { get; set; }
+    }
+
+    /// <summary>
+    /// 池对象缓存约束
+    /// </summary>
+    public interface IPoolType
+    {
+        void Recycle2Cache();
+    }
+
+    /// <summary>
+    /// 安全的对象池
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class SafeObjectPool<T> : Pool<T>,ISingleton where T : IPoolable, new()
+    {
+        #region 单例
+        void ISingleton.OnSingletonInit()
+        {
+
+        }
+
+        private SafeObjectPool()
+        {
+            mFactory = new DefaultObjectFactory<T>();
+        }
+
+        public static SafeObjectPool<T> Instance
+        {
+            get
+            {
+                return SingletonProperty<SafeObjectPool<T>>.Instance;
+            }
+        }
+
+        public void Dispose()
+        {
+            SingletonProperty<SafeObjectPool<T>>.Dispose();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 对象池的最大容量
+        /// </summary>
+        /// <value>The max cache count.</value>
+        public int MaxCacheCount
+        {
+            get { return mMaxCount; }
+            set
+            {
+                mMaxCount = value;
+
+                if (mCacheStack != null)
+                {
+                    if (mMaxCount > 0)
+                    {
+                        if (mMaxCount < mCacheStack.Count)
+                        {
+                            int removeCount = mCacheStack.Count - mMaxCount;
+                            while (removeCount > 0)
+                            {
+                                mCacheStack.Pop();
+                                --removeCount;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 初始化对象池的最大容量和初始容量
+        /// </summary>
+        /// <param name="maxCount">最大容量</param>
+        /// <param name="initCount">初始容量</param>
+        public void Init(int maxCount,int initCount)
+        {
+            MaxCacheCount = maxCount;
+
+            if (maxCount > 0)
+            {
+                initCount = Math.Min(maxCount, initCount);
+            }
+
+            if(CurCount< initCount)
+            {
+                for (int i = CurCount; i < initCount; i++)
+                {
+                    Recycle(new T());
+                }
+            }
+        }
+
+        public override T Allocate()
+        {
+            T result = base.Allocate();
+            result.IsRecycled = false;
+            return result;
+        }
+
+        public override bool Recycle(T obj)
+        {
+            if (obj == null || obj.IsRecycled)
+            {
+                return false;
+            }
+
+            if (mMaxCount > 0)
+            {
+                if (mCacheStack.Count >= mMaxCount)
+                {
+                    obj.OnRecycled();
+                    return false;
+                }
+            }
+
+            obj.IsRecycled = true;
+            obj.OnRecycled();
+            mCacheStack.Push(obj);
+
+            return true;
+        }
+    }
+    #endregion
+
+
+}
---
+++ Assets/EFrameExample/ObjectPool/EFrameExample_ObjectPool.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EFrame.Example
+{
+    public class EFrameSimpleMsg
+    {
+        public string Msg = "";
+
+        public void Reset()
+        {
+            Msg = "";
+        }
+    }
+
+    public class EFrameSafeMsg:IPoolType,IPoolable
+    {
+
+        public string Msg = "";
+
+        #region IPoolable接口实现
+        public bool IsRecycled { get; set; }
+
+        public void OnRecycled()
+        {
+            Msg = "";
+            Debug.Log("回收到对象池进行，重新进行初始化操作");
+        }
+        #endregion
+
+
+        #region IPoolType接口实现
+
+        public static EFrameSafeMsg Allocate()
+        {
+            return SafeObjectPool<EFrameSafeMsg>.Instance.Allocate();
+        }
+
+        public void Recycle2Cache()
+        {
+            SafeObjectPool<EFrameSafeMsg>.Instance.Recycle(this);
+        }
+
+        #endregion
+
+
+    }
+
+    public class EFrameExample_ObjectPool : MonoBehaviour
+    {
+        private void Start()
+        {
+
+            #region 简易对象池示例
+            SimpleObjectPool<EFrameSimpleMsg> simpleMsgPool = new SimpleObjectPool<EFrameSimpleMsg>(
+                () => new EFrameSimpleMsg(),
+                simpleMsg => { simpleMsg.Reset(); },
+                10
+                );
+
+            Debug.Log(string.Format("simpleMsgPool.CurCount: {0}", simpleMsgPool.CurCount));
+
+            var t_simpleMsg = simpleMsgPool.Allocate();
+
+            Debug.Log(string.Format("simpleMsgPool.CurCount: {0}", simpleMsgPool.CurCount));
+
+            var res = simpleMsgPool.Recycle(t_simpleMsg);
+
+            Debug.Log(string.Format("simpleMsgPool.CurCount: {0}", simpleMsgPool.CurCount));
+
+            EFrameSimpleMsg[] msgs = new EFrameSimpleMsg[5];
+
+            for (int i = 0; i < msgs.Length; i++)
+            {
+                msgs[i] = simpleMsgPool.Allocate();
+            }
+
+            Debug.Log(string.Format("simpleMsgPool.CurCount: {0}", simpleMsgPool.CurCount));
+
+            for (int i = 0; i < msgs.Length; i++)
+            {
+                simpleMsgPool.Recycle(msgs[i]);
+            }
+
+            Debug.Log(string.Format("simpleMsgPool.CurCount: {0}", simpleMsgPool.CurCount));
+            #endregion
+
+            #region 安全对象池示例
+            SafeObjectPool<EFrameSafeMsg> safeMsgPool = SafeObjectPool<EFrameSafeMsg>.Instance;
+
+            safeMsgPool.Init(100, 50);
+
+            Debug.Log(string.Format("safeMsgPool.CurCount: {0}", safeMsgPool.CurCount));
+
+            var msg = EFrameSafeMsg.Allocate();
+
+            Debug.Log(string.Format("safeMsgPool.CurCount: {0}", safeMsgPool.CurCount));
+
+            msg.Recycle2Cache();
+
+            Debug.Log(string.Format("safeMsgPool.CurCount: {0}", safeMsgPool.CurCount));
+
+            EFrameSafeMsg[] safeMsgs = new EFrameSafeMsg[40];
+
+            for (int i = 0; i < safeMsgs.Length; i++)
+            {
+                safeMsgs[i] = EFrameSafeMsg.Allocate();
+            }
+
+            Debug.Log(string.Format("safeMsgPool.CurCount: {0}", safeMsgPool.CurCount));
+
+            for (int i = 0; i < safeMsgs.Length; i++)
+            {
+                safeMsgs[i].Recycle2Cache();
+            }
+
+            Debug.Log(string.Format("safeMsgPool.CurCount: {0}", safeMsgPool.CurCount));
+
+            #endregion
+
+        }
+    }
+}
+
+
