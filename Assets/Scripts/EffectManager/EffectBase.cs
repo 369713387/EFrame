51b9686f397660fb4125cf23a2b896abe7dfcf4c
369713387 <369713387@qq.com>
2021-12-09 16:39:23 +0800

    特效管理工具

---
+++ Assets/Scripts/EffectManager/EffectBase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectBase : MonoBehaviour, I_PoolItem
+{
+    public string res_Path { get { return "Prefab/" + gameObject.name.Substring(0, gameObject.name.Length - 7); } }
+
+    public float time = 1f;
+
+    public void Dispose()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    protected virtual void OnEnable()
+    {
+        StartCoroutine(IE_AutoDispose());
+    }
+
+    IEnumerator IE_AutoDispose()
+    {
+        yield return new WaitForSeconds(time);
+
+        EffectManager.Instance.RecycleEffect(this);
+    }
+}
---
+++ Assets/Scripts/EffectManager/EffectManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using YiFun;
+
+namespace Yifun
+{
+    public class EffectManager : MonoBehaviour
+    {
+        #region 单例
+        private static EffectManager instance;
+
+        public static EffectManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    GameObject obj = GameObject.Find(typeof(EffectManager).Name);
+                    instance = obj.GetComponent<EffectManager>();
+                    DontDestroyOnLoad(obj);                    
+                }
+                return instance;
+            }
+        }
+        #endregion
+
+        public const string effect_Path = "Prefab/Effect";
+
+        public List<string> list_key = new List<string>();
+
+        public Dictionary<string, MonoPool<EffectBase>> dic = new Dictionary<string, MonoPool<EffectBase>>();
+
+        private void Awake()
+        {
+            list_key = new List<string>() { effect_Path };
+
+            for (int i = 0; i < list_key.Count; i++)
+            {
+                MonoPool<EffectBase> pool = new MonoPool<EffectBase>(list_key[i], transform, 5);
+                pool.InitPool();
+                dic.Add(list_key[i], pool);
+            }
+        }
+        public void CreateEffect(string key, Vector3 position)
+        {
+            EffectBase go = dic[key].AllocatePoolItem(transform);
+            go.transform.position = position;
+        }
+
+        public void RecycleEffect(EffectBase effect)
+        {
+            dic[effect.res_Path].ResetPoolItem(effect);
+        }
+    }
+}
+
+
